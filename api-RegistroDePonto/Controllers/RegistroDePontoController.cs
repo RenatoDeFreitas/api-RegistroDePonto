f0dc80b48c35f212107ab9038e570c79ce8b648b
Renato Gualberto <gprenatofreitas@gmail.com>
2024-04-03 15:59:20 -0300

    Criado controlador para registro de ponto

---
+++ api-RegistroDePonto/Controllers/RegistroDePontoController.cs
@@ -0,0 +1,27 @@
+﻿using api_RegistroDePonto.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_RegistroDePonto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistroDePontoController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public RegistroDePontoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAll()
+        {
+            var model = await _context.RegistroDePonto.ToListAsync();
+
+            return Ok(model);
+        }
+    }
+}
