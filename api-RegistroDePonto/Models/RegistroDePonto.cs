4634949d68cbb53ea12106f6039ea9ae407bddab
Renato Gualberto <gprenatofreitas@gmail.com>
2024-04-03 10:41:14 -0300

    Criado  pasta Models + classe  "Registrode ponto"

---
+++ api-RegistroDePonto/Models/RegistroDePonto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api_RegistroDePonto.Models
+{
+    [Table("Registro")]
+    public class RegistroDePonto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string NomeFuncionario { get; set; }
+        [Required]
+        public DateTime EntradaManha { get; set; }
+        [Required]
+        public DateTime SaidaManha { get; set; }
+        [Required]
+        public DateTime EntradaTarde { get; set; }
+        [Required]
+        public DateTime SaidaTarde { get; set; }
+
+    }
+}
